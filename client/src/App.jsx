76853cce91885f865c5355153f47fb774d3707b0
Atyn97 <fatin.nooraina97@gmail.com>
2023-12-11 18:53:35 +0100

    edit url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = "https://margo-chat-server.vercel.app/";
+  axios.defaults.baseURL = "https://margo-chat-server.vercel.app";
   axios.defaults.withCredentials = true;
 
   return (
