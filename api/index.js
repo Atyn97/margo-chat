e84dc88a59cd2cb5e8d28a47bd81ccfeda3856c3
Atyn97 <fatin.nooraina97@gmail.com>
2023-12-11 16:35:33 +0100

    Edit  index.js

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(function (request, response, next) {
   response.header(
     "Access-Control-Allow-Origin",
-    "https://margo-chat-server.vercel.app/"
+    "https://margo-chat.vercel.app/"
   );
   response.header(
     "Access-Control-Allow-Headers",
